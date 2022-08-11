62680aeb6375aeea914b07f7d3246c89d5706126
WorldCreator46 <96541154+WorldCreator46@users.noreply.github.com>
2022-08-11 20:11:07 +0900

    회계
    음

---
+++ Path.py
@@ -0,0 +1,4 @@
+Assets_Path = "Assets/"
+Image_Path = "Images/"
+
+Icon_Path = f"{Assets_Path}{Image_Path}AccountingProgramIcon.png"
---
+++ main.py
@@ -0,0 +1,19 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
+from Path import *
+
+class MyApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
+    def init_ui(self):
+        self.setWindowTitle('Icon')
+        self.setWindowIcon(QIcon(Icon_Path))
+        self.setGeometry(300, 300, 300, 200)
+        self.show()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
